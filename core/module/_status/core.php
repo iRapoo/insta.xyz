50baee87259641f2da586e7d5f0c3322f1e8318a
Rapoo <my.xperia.like@gmail.com>
2017-09-16 14:03:39 +0300

    Added view @test for bench load page

--- core/module/_status/core.php
+++ core/module/_status/core.php
@@ -9,11 +9,13 @@
 $_devPanel = new Kernel();
 $_devPanel->_setHtml(PATH.'/core/module/_status/panel.tpl.html');
 
+$_devPanel->_setVar("module_path", PATH.'/core/module/_status/');
+
 $_devPanel->_setVar("http_response_code", http_response_code());
 $_devPanel->_setVar("php_version", phpversion());
 $_devPanel->_setVar("proj_version", $_config->_getManifest()->base->version);
 $_devPanel->_setVar("load_speed", $_config->_endPage($start));
 $_devPanel->_setVar("this_view", _VIEW_);
+$_devPanel->_setVar("data_base", $_config->_getManifest()->db_config->db_name);
 
-if($_config->_getManifest()->base->devPanel)
-    $BODY .= $_devPanel->_getHtml();
+$BODY .= $_devPanel->_getHtml();
--- views/controller.php
+++ views/controller.php
@@ -30,5 +30,7 @@ if(file_exists(_DIR_._VIEW_.'/core.php'))
     include_once(_DIR_._VIEW_.'/core.php');
 }
 
-include_once(PATH.'/core/module/_status/core.php');
+if($_config->_getManifest()->base->devPanel)
+    include_once(PATH.'/core/module/_status/core.php');
+
 include_once(PATH.'/core/base.php');
