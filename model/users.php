16c99858428bb44796882e57fa96309323dd19a7
Rapoo <my.xperia.like@gmail.com>
2017-09-22 19:26:43 +0300

    Create registration veiw

--- model/users.php
+++ model/users.php
@@ -3,6 +3,7 @@
 class users extends Atom
 {
     public $id;
+    public $login;
     public $birthday;
     public $email;
     public $password;
--- views/registration/core.php
+++ views/registration/core.php
@@ -6,8 +6,11 @@ $_config->css[] = _ASSETS_."/css/reg.css";
 
 $_config->js[] = _ASSETS_."/js/bootstrap-libs/bootstrap-formhelpers-phone.js";
 
-echo $_POST['firstName'];
+$access = (empty($_POST)) ? true : false;
 
 $html = new Kernel();
 $html->_setHtml(_DIR_._VIEW_."/reg.tpl.html");
+
+$html->_setIf("post", $access);
+
 $_config->body .= $html->_getHtml();
