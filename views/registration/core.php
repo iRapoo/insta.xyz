42fad314ed1925075c4c7497a487de4f7bbfcb35
Rapoo <my.xperia.like@gmail.com>
2017-09-23 12:10:29 +0300

    Registration finished

--- views/registration/core.php
+++ views/registration/core.php
@@ -1,16 +1,48 @@
 <?php
 
+Atom::setup($_config->_getMySQLi());
+Atom::model("users");
+
 $_config->title = "Регистрация";
 
 $_config->css[] = _ASSETS_."/css/reg.css";
-
 $_config->js[] = _ASSETS_."/js/bootstrap-libs/bootstrap-formhelpers-phone.js";
+$_config->js[] = _ASSETS_."/js/_regValid.js";
 
 $access = (empty($_POST)) ? true : false;
 
+switch($_GET['status']){
+    case "ok":
+        $success = "<font color='#006400'>Регистрация прошла успешно!</font>";
+        break;
+    case "error":
+        $success = "<font color='#8b0000'>Ошибка регистрации, повторите позже...</font>";
+        break;
+}
+
+/*
+if (hash_equals($hashed_password, crypt($input_password, $hashed_password))) {
+    echo "Пароль верен!";
+}*/
+
+if(!$access) {
+    $user = new users();
+    foreach ($_POST as $key => $value) {
+
+        if($key!="password")
+            $user->$key = $value;
+        else
+            $user->$key = crypt($value);
+
+    }
+    if($user->insert())
+        header("location: ".$_SERVER['REQUEST_URI']."?status=ok");
+    else
+        header("location: ".$_SERVER['REQUEST_URI']."?status=error");
+}
+
 $html = new Kernel();
 $html->_setHtml(_DIR_._VIEW_."/reg.tpl.html");
-
-$html->_setIf("post", $access);
-
+$html->_setIf("post", (empty($_GET['status'])));
+$html->_setVar("result_registration", $success);
 $_config->body .= $html->_getHtml();
