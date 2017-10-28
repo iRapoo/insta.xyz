dcf75280255b0a3c33e9737b11660594257f5b66
Rapoo <my.xperia.like@gmail.com>
2017-10-28 16:21:23 +0300

    Реализована главная страница и добавлены разделы/подразделы

--- views/base/core.php
+++ views/base/core.php
@@ -1,12 +1,47 @@
 <?php
 
-//************************** HEADERS *******************************//
+Atom::setup($_config->_getMySQLi());
+Atom::model("category");
+Atom::model("subsections");
 
-$_config->title = "Welcome! ".$_config->_getManifest()->base->name; //SET TITLE
-$_config->css[] = _ASSETS_."/css/base.css"; //SET STYLESHEET
+$category = category::findAll();
 
-//**************************** END *******************************//
+$_config->title = $_config->_getManifest()->base->name.". Каталог";
+$_config->css[] = _ASSETS_."/css/base.css";
 
 $html = new Kernel();
-$html->_setHtml(_DIR_._VIEW_.'/welcome.tpl.html');
+$html->_setHtml(_DIR_._VIEW_.'/main.tpl.html');
+
+$menu = "";
+if(!empty($category)) {
+    foreach ($category as $_cat) {
+        if (!empty($_cat->id)) {
+
+            $nav_menu = new Kernel();
+            $nav_menu->_setHtml(_DIR_._VIEW_."/menu.tpl.html");
+
+            $nav_menu->_setVar("menu_name", $_cat->name);
+
+            $subsections = subsections::findAll("WHERE `cid` = '{$_cat->id}'");
+
+            $menu_subs = "";
+            if(!empty($subsections)) {
+                foreach ($subsections as $_sub) {
+                    if (!empty($_sub->id)) {
+
+                        $menu_subs .= '<li><a href="/sub?type='.$_sub->id.'">'.$_sub->name.'</a></li>';
+
+                    }
+                }
+            }
+
+            $nav_menu->_setVar("menu_subs", $menu_subs);
+
+            $menu .= $nav_menu->_getHtml();
+        }
+    }
+}
+
+$html->_setVar("nav_menu", $menu);
+
 $_config->body .= $html->_getHtml();
